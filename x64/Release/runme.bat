cfdb25ffce04275a068738c1e4538e468463bfa2
taiden26l <134104374+taiden26l@users.noreply.github.com>
2026-06-30 16:54:32 -0600

    Delete x64/Release directory

--- x64/Release/runme.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set SCRIPT=%~dp0setup.ps1
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-"Start-Process PowerShell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%SCRIPT%""'"
